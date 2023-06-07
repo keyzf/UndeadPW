a5d7e95f66e2ccc26e03055fee1251cb69ff5fa2
im-ayakovtsov <ayakovtsov@interfirst.com>
2023-06-07 12:32:31 +0200

    fix addressFragment

--- src/apply/fragments/addressFragment.ts
+++ src/apply/fragments/addressFragment.ts
@@ -7,28 +7,28 @@ export class AddressFragment {
     this.page = page;
   }
 
-    /**
+  /**
    * @param address - address
    * @param zipCode - address
    * @param clear - clear field or not (by default = false)
    */
   async enterAddress({address, zipCode, clear = false}: {address?: string, zipCode?: string, clear?: boolean}) {
-      if(address) {
-        if(clear) {
-          await this.page.locator('[data-testid="address__streetAddressInput"]').clear();
-          await this.page.locator('[data-testid="address__streetAddressInput"]').type(address, {delay: 30});
-          await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
-        }
+    if(address) {
+      if(clear) {
+        await this.page.locator('[data-testid="address__streetAddressInput"]').clear();
         await this.page.locator('[data-testid="address__streetAddressInput"]').type(address, {delay: 30});
         await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
-      } else if(zipCode) {
-        if(clear) {
-          await this.page.locator('[name="zip"]').clear();
-          await this.page.locator('[name="zip"]').type(zipCode, {delay: 30});
-          await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
-        }
+      }
+      await this.page.locator('[data-testid="address__streetAddressInput"]').type(address, {delay: 30});
+      await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
+    } else if(zipCode) {
+      if(clear) {
+        await this.page.locator('[name="zip"]').clear();
         await this.page.locator('[name="zip"]').type(zipCode, {delay: 30});
         await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
       }
+      await this.page.locator('[name="zip"]').type(zipCode, {delay: 30});
+      await this.page.locator('.pac-container').last().locator('[class="pac-item"]').first().click();
+    }
   }
 }
